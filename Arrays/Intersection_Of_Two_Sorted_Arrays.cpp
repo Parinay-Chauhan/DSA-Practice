249d8116878c8bb28382ad2c3fc46557616dc10b
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-07-04 22:24:47 +0530

    Add Intersection Of Two Sorted Arrays solution

---
+++ Arrays/Intersection_Of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,12 @@
+/*
+Platform : Coding Ninjas
+Problem : Intersection Of Two Sorted Arrays
+Difficulty : Easy
+Approach : Two Pointer
+Time Complexity : O(n + m)
+Space Complexity : O(1)
+Time Limit: 1 sec
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
